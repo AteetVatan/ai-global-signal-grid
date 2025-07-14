25be3be4397975d9f7aebf7b555356cdfe23b6fe
ateetvatan <ateetph@gmail.com>
2025-07-14 20:55:08 +0200

    for google translate language code using variant for only chinese -    "zh": ["zh-CN", "zh-TW", "zh-HK"]

--- src/app/constants/google_translate_variants.py
+++ src/app/constants/google_translate_variants.py
@@ -1,13 +1,13 @@
 GOOGLE_TRANSLATE_VARIANTS = {
   "zh": ["zh-CN", "zh-TW", "zh-HK"],
-  "pt": ["pt", "pt-PT", "pt-BR"],
-  "es": ["es", "es-419", "es-ES", "es-US", "es-MX", "es-AR", "es-CO", "es-CL", "es-PE", "es-VE", "es-DO", "es-CU"],
-  "en": ["en", "en-US", "en-GB", "en-AU", "en-IN", "en-CA", "en-NZ", "en-ZA", "en-SG"],
-  "fr": ["fr", "fr-FR", "fr-CA", "fr-BE", "fr-CH", "fr-LU"],
-  "de": ["de", "de-DE", "de-AT", "de-CH"],
-  "it": ["it", "it-IT", "it-CH"],
-  "sv": ["sv", "sv-SE", "sv-FI"],
-  "nl": ["nl", "nl-NL", "nl-BE"],
-  "no": ["no", "nb", "nn"],
+  #"pt": ["pt", "pt-PT", "pt-BR"],
+  #"es": ["es", "es-419", "es-ES", "es-US", "es-MX", "es-AR", "es-CO", "es-CL", "es-PE", "es-VE", "es-DO", "es-CU"],
+  #"en": ["en", "en-US", "en-GB", "en-AU", "en-IN", "en-CA", "en-NZ", "en-ZA", "en-SG"],
+  #"fr": ["fr", "fr-FR", "fr-CA", "fr-BE", "fr-CH", "fr-LU"],
+  #"de": ["de", "de-DE", "de-AT", "de-CH"],
+  #"it": ["it", "it-IT", "it-CH"],
+  #"sv": ["sv", "sv-SE", "sv-FI"],
+  #"nl": ["nl", "nl-NL", "nl-BE"],
+  #"no": ["no", "nb", "nn"],
   "ta": ["ta-IN", "ta-LK"]
 }
