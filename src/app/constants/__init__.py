d87d460fb3a7203ff55ea585e0f7063115c182e0
ateetvatan <ateetph@gmail.com>
2025-07-25 17:39:30 +0200

    country files to init

--- src/app/constants/__init__.py
+++ src/app/constants/__init__.py
@@ -16,13 +16,15 @@
 #
 # Contact: ab@masxai.com | MASXAI.com
 
-from .country import COUNTRY_VARIATIONS
+from .country_variations import COUNTRY_VARIATIONS
+from .country_v2 import CountryV2Manager
 from .iso_language import ISO_REGION_LANGUAGES
 from .iso_to_nllb import ISO_TO_NLLB_MERGED
 from .google_translate_variants import GOOGLE_TRANSLATE_VARIANTS
 from .domains import DOMAIN_CATEGORIES  
 
 __all__ = [
+    CountryV2Manager,
     COUNTRY_VARIATIONS,
     ISO_REGION_LANGUAGES,
     ISO_TO_NLLB_MERGED,
